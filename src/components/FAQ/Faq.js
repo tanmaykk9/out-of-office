2a7a3699776b6580135daa8c0899bde278909a4e
komal <knale@intervision.com>
2022-11-01 13:27:32 +0530

    faq component addded

---
+++ src/components/FAQ/Faq.js
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+const Faq = () => {
+  return (
+    <div>
+      komal
+    </div>
+  );
+};
+
+export { Faq };
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar } from "../../components/Navbar";
 import { Banner } from "../../components/Banner/Banner";
+import { Faq } from "../../components/FAQ";
 
 const Homepage = () => {
   return (
     <div className="main-app">
       <Navbar />
       <Banner />
+      <Faq/>
     </div>
   );
 };
